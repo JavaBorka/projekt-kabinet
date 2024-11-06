06f61c94aabaa3326f64c291fa644978b0db1d8b
JavaBorka <borka34@gmail.com>
2024-11-06 02:06:18 +0100

    grid library

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.log("ok")
+
+////////////////////////////////////////////////////
+// Knihovna Masonry pro rozložení článků do grigu //
+///////////////////////////////////////////////////
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
