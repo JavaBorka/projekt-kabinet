35ef227567e9b01a2af35d8280a22d0a27088e58
JavaBorka <borka34@gmail.com>
2025-01-21 15:10:20 +0100

    gsap animations library installed

--- vite-project-kabinet/src/Content/GenrePaginatedList.jsx
+++ vite-project-kabinet/src/Content/GenrePaginatedList.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { PaginationNav } from "./PaginationNav.jsx";
 import { ITEMS_PER_PAGE } from "../constants/itemsPerPage.constants.js";
 import { getPostsPreviewGenre } from "../constants/getPostsPreviewGenre.js";
-import Masonry from "react-responsive-masonry";
+// import Masonry from "react-responsive-masonry";
 
 export const GenrePaginatedList = ({catId}) => {
 
@@ -28,12 +28,10 @@ export const GenrePaginatedList = ({catId}) => {
     return (
         <>
             <section className="card__container">
-                <Masonry>
                     {items
                         .map((item) => (
                         <CardPreview key={item.id} id={item.id} genre={item.genre} title={item.title} author={item.author} content={item.perex} image={item.image}/>
                     ))}
-                </Masonry>
             </section>
 
             <PaginationNav currentPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange}/>
