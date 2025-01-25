353a7869727319183fa44152efc6887291e69813
JavaBorka <borka34@gmail.com>
2025-01-25 14:08:23 +0100

    set min-height for card__container

--- vite-project-kabinet/src/constants/WPcategories.constants.js
+++ vite-project-kabinet/src/constants/WPcategories.constants.js
@@ -6,3 +6,6 @@ export const WP_CAT_ID_DIARY = 12
 export const WP_CAT_ID_INTERVIEW = 14
 export const WP_CAT_ID_REVIEW = 13
 export const CAT_ID_TEXTS = "texty"
+
+// vytvoriť objekt z diara: WP_CAT_ID_ESSAY... a potom ho naimportovať v komponente Wrapper
+// const catID = WP_CAT[genre]
