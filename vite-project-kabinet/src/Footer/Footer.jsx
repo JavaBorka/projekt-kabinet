1b94f95fa4004618958679c80f8ad25b23589296
JavaBorka <borka34@gmail.com>
2025-01-26 18:21:49 +0100

    footer update as per owner request

--- vite-project-kabinet/src/Footer/Footer.jsx
+++ vite-project-kabinet/src/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
             <div className="fond__container">
                 <div className="fond__text-container">
                     <p className="fond__content">Časopis z verejných zdrojov podporil</p>
-                    <a className="fond__link" href="#">
+                    <a className="fond__link" href="https://www.fpu.sk/sk/">
                         <img className="fond__image" src={fpu} alt="Fond na podporu umenia logo" />
                     </a>
                     <p className="fond__issn">ISSN 2729-7993</p>
