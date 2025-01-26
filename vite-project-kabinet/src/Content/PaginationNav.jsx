ed011ba4061977792eeeaddd7ed4c8fed51ac2a4
JavaBorka <borka34@gmail.com>
2025-01-27 00:19:59 +0100

    page navigation and css update as per owner request

--- vite-project-kabinet/src/Content/PaginationNav.jsx
+++ vite-project-kabinet/src/Content/PaginationNav.jsx
@@ -5,38 +5,40 @@ import "./paginated.style.css"
 export const PaginationNav = ({currentPage, totalPages, handlePageChange}) => {
 
     return (
-        <div className="pages_pc">
-            {currentPage >= 1 &&
-                <button
-                    className="btn btn-desktop btn-left"
-                    onClick={() => {
-                        handlePageChange(currentPage - 1);
-                        document.getElementById('top').focus()
-                    }}
-                    disabled={currentPage === 1}
-                >
-                    <img src={arrowLeft} alt="šípka vľavo"/>
-                </button>
-            }
+        <div className="pages__container">
+            <div className="pages_pc">
+                {currentPage >= 1 &&
+                    <button
+                        className="btn btn-desktop btn-left"
+                        onClick={() => {
+                            handlePageChange(currentPage - 1);
+                            document.getElementById('top').focus()
+                        }}
+                        disabled={currentPage === 1}
+                    >
+                        <img src={arrowLeft} alt="šípka vľavo"/>
+                    </button>
+                }
 
-            <ul className="pages__numbers">
-                <li>
-                    <button className="btn btn-desktop btn-number">{currentPage}</button>
-                </li>
-            </ul>
+                <ul className="pages__numbers">
+                    <li>
+                        <button className="btn btn-desktop btn-number">{currentPage}</button>
+                    </li>
+                </ul>
 
-            {currentPage <= totalPages && 
-                <button
-                    className="btn btn-desktop btn-right"
-                    onClick={() => {
-                        handlePageChange(currentPage + 1);
-                        document.getElementById('top').focus()
-                    }}
-                    disabled={currentPage === totalPages}
-                >
-                    <img src={arrowRight} alt="šípka vpravo"/>
-                </button>
-            }
+                {currentPage <= totalPages && 
+                    <button
+                        className="btn btn-desktop btn-right"
+                        onClick={() => {
+                            handlePageChange(currentPage + 1);
+                            document.getElementById('top').focus()
+                        }}
+                        disabled={currentPage === totalPages}
+                    >
+                        <img src={arrowRight} alt="šípka vpravo"/>
+                    </button>
+                }
+            </div>
         </div>
     )
 }
