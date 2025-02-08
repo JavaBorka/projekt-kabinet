89e7301a840fedd98ac62a8017d7c660bfbbd5ce
JavaBorka <borka34@gmail.com>
2025-02-08 18:28:09 +0100

    vite config for Netlify

--- vite-project-kabinet/vite.config.js
+++ vite-project-kabinet/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
+  build: {
+    outDir: 'dist', // Build výstup se vytvoří v podsložce 'dist' uvnitř 'vite-project-kabinet'
+  },
   plugins: [react()],
 })
