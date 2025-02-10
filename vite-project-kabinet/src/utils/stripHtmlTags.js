99c42a71c21f56fc8e1cca1fe6cad7acab7dc218
JavaBorka <borka34@gmail.com>
2025-02-10 23:22:17 +0100

    stripHTMLtags regular expression changed to DOMParser

--- vite-project-kabinet/src/utils/stripHtmlTags.js
+++ vite-project-kabinet/src/utils/stripHtmlTags.js
@@ -1,1 +1,8 @@
-export const stripHTMLTags = str => str.replace(/<[^>]*>/g, '');
+// Regulární výraz neošetří případy, když jsou v textu tři tečky ...
+// export const stripHTMLTags = str => str.replace(/<[^>]*>/g, '');
+
+export const stripHTMLTags = html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+  }
