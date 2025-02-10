a85b4570f3589fecefb57cedfe118b43ded298cb
JavaBorka <borka34@gmail.com>
2025-02-10 15:18:14 +0100

    medium-sized images changed to large-sized images

--- vite-project-kabinet/src/constants/getImageSrc.js
+++ vite-project-kabinet/src/constants/getImageSrc.js
@@ -2,7 +2,7 @@ export const getImageSrc = (post) => {
     const arrayEmbedded = post._embedded["wp:featuredmedia"] ? post._embedded["wp:featuredmedia"] : []
 
     if (arrayEmbedded.length > 0) {
-        const image = arrayEmbedded[0]?.media_details?.sizes?.medium?.source_url;
+        const image = arrayEmbedded[0]?.media_details?.sizes?.large?.source_url;
         return image
     }
     else {
