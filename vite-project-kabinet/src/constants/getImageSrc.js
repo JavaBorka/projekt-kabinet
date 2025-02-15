c7c75192bf3b1101056d2c8b939fad4b0e16c229
JavaBorka <borka34@gmail.com>
2025-02-15 21:15:46 +0100

    get full sized image when large image is not available

--- vite-project-kabinet/src/constants/getImageSrc.js
+++ vite-project-kabinet/src/constants/getImageSrc.js
@@ -2,8 +2,9 @@ export const getImageSrc = (post) => {
     const arrayEmbedded = post._embedded["wp:featuredmedia"] ? post._embedded["wp:featuredmedia"] : []
 
     if (arrayEmbedded.length > 0) {
-        const image = arrayEmbedded[0]?.media_details?.sizes?.large?.source_url;
-        return image
+        const imageLarge = arrayEmbedded[0]?.media_details?.sizes?.large?.source_url;
+        const imageFull = arrayEmbedded[0]?.media_details?.sizes?.full?.source_url;
+        return imageLarge ? imageLarge : imageFull
     }
     else {
         return null;
