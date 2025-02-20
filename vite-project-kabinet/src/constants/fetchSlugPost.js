91361800c2ef95b8a5d7a8b019c55a075485fdfc
JavaBorka <borka34@gmail.com>
2025-02-21 00:01:49 +0100

    fetchSlugPost after review

--- vite-project-kabinet/src/constants/fetchSlugPost.js
+++ vite-project-kabinet/src/constants/fetchSlugPost.js
@@ -2,7 +2,7 @@ import { BASE_API_URL } from "./WPbaseAPI.constants";
 
 export const fetchSlugPost = async (slug) => {
 
-    const reqSlugPost = await fetch(`${BASE_API_URL}/posts?_embed&slug=${slug}`)
+    const reqSlugPost = await fetch(`${BASE_API_URL}/posts?&slug=${slug}`)
     const slugPost = await reqSlugPost.json()
     
     return slugPost
